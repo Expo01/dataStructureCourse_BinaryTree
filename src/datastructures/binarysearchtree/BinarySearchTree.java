5f961b7bf8195d35746274ecd372635ebd16836a
Expo <crdailey01@gmail.com>
2023-03-15 21:47:53 -0700

    annotated insert()

--- src/datastructures/binarysearchtree/BinarySearchTree.java
+++ src/datastructures/binarysearchtree/BinarySearchTree.java
@@ -16,21 +16,23 @@ public class BinarySearchTree {
     }
 
     public boolean insert(int value) {
-        Node newNode = new Node(value);
-        if (root == null) {
+        Node newNode = new Node(value); //node created but not part of tree
+        if (root == null) { //if tree is empty, new node is the root
             root = newNode;
             return true;
         }
         Node temp = root;
-        while (true) {
-            if (newNode.value == temp.value) return false;
-            if (newNode.value < temp.value) {
-                if (temp.left == null) {
-                    temp.left = newNode;
+        while (true) { //will ultimateley run until its ablee to place the newe node and return true, or return false indicating
+            // the value already exists in the tree
+            if (newNode.value == temp.value) return false; // handles edge case of newNode value already existing in tree. loop ends.
+            if (newNode.value < temp.value) {  //checks it value is less than temp
+                if (temp.left == null) { //if is less than temp left pointer null
+                    temp.left = newNode; //then newNode added as leaf
                     return true;
                 }
-                temp = temp.left;
-            } else {
+                temp = temp.left; // if newNode is less than temp but left pointer of temp is not null (node already
+                // exists) then temp reassigned to next node down the tree to the left
+            } else { //same concepts except for value that is greater than temp
                 if (temp.right == null) {
                     temp.right = newNode;
                     return true;
